df6ea912473b817374c50ec69889599d0b7768f5
Jorge Anzola <jorgeanzola@gmail.com>
2025-01-31 13:52:54 +0100

    Added Spanish translations

---
+++ src/translations/es/batch-actions.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Are you sure you want to delete the selected blocks?' => '¿Estás seguro que quieres eliminar los bloques selccionados?',
+    'Collapse' => 'Colapsar',
+    'Copy' => 'Copiar',
+    'Delete' => 'Eliminar',
+    'Disable' => 'Desactivar',
+    'Enable' => 'Activar',
+    'Expand' => 'Expandir',
+    'Paste' => 'Pegar',
+    'Select all' => 'Seleccionar todos',
+];
